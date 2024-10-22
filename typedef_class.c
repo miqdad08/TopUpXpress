e1a235fafa6ca9eb7529d2ade03b8d0b8f582f7d
Miqdad <44460047+miqdad08@users.noreply.github.com>
2024-10-22 17:38:43 +0700

    Create typedef_class.c

---
+++ typedef_class.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+typedef struct {
+    char name[50];
+} Game;
+
+
+typedef struct {
+    int diamondQty;
+    int price;
+    char currencyType[50];
+} PackageItem;
+
+typedef struct {
+    char name[50];
+} PaymentMethod;
